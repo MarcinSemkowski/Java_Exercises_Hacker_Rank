9f0545c4821d5671d6364241af77d6c6f42ffa25
Marcin <marcin96semkowski@gmail.com>
2019-10-18 17:37:58 +0200

    Java Comparator

---
+++ src/HackerRank/JavaComparator/Checker.java
@@ -0,0 +1,14 @@
+package HackerRank.JavaComparator;
+
+import java.util.Comparator;
+
+public class Checker implements Comparator<Player> {
+    @Override
+    public int compare(Player o1, Player o2) {
+        if(o1.score == o2.score){
+         return o1.name.compareTo(o2.name);
+        }
+       return  Integer.compare(o2.score,o1.score);
+
+    }
+}
---
+++ src/HackerRank/JavaComparator/Player.java
@@ -0,0 +1,11 @@
+package HackerRank.JavaComparator;
+
+class Player{
+    String name;
+    int score;
+
+    Player(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
